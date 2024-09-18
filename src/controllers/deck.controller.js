5bec11f2362a69926afbfa18f01c79678031ecc1
JacobCll <jacobcarbonell06@gmail.com>
2024-09-18 21:08:06 +0800

    nothing much

--- src/controllers/deck.controller.js
+++ src/controllers/deck.controller.js
@@ -1,5 +1,6 @@
 const httpStatus = require("http-status");
 const { CARD_IDS } = require("../assets/cards.data");
+const Deck = require("../models/deck");
 
 /*
     DECKS are an array of IDs 
@@ -11,11 +12,19 @@ const createDeck = (req, res) => {
     code: status,
     message: "Deck successfully created",
   };
+
   res.status(status).json(response);
 };
 
-const showDeck = (req, res) => {
+const showDeck = async (req, res) => {
   const status = httpStatus.OK;
+  const deckID = req.params.deckID;
+  const deckResult = await Deck.findOne({ _id: deckID });
+  const response = {
+    code: status,
+    body: deckResult,
+  };
+  res.status(status).json(response);
 };
 
 module.exports = { createDeck, showDeck };
--- src/models/deck.js
+++ src/models/deck.js
@@ -2,4 +2,11 @@ const mongoose = require("mongoose");
 
 const deckSchema = mongoose.Schema({
   _id: mongoose.Schema.Types.ObjectId,
+  cards: [
+    {
+      type: String,
+    },
+  ],
 });
+
+module.exports = mongoose.model("Deck", deckSchema);
